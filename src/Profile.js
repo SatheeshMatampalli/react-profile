1e5485a4d78e11fc9e54f8368cfe20d3b8939cf9
matampalli satheesh <m.satheesh566@gmail.com>
2020-04-28 15:07:31 +0530

    react first commit

---
+++ src/Profile.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './App.css';
+import Data from './data/data.json';
+
+class Profile extends React.Component{
+    render(){
+        var indexValue=this.props.location.data.id;
+        var specificProfile=Data.profiles[indexValue];
+        return(
+            <section className="parent">
+                <div className="card"> 
+                    <div className="card-top">
+        <h3> {specificProfile.basicInformation.name} </h3>
+    <em> {specificProfile.basicInformation.email} </em>
+                     </div>
+
+                     <div className="card-bottom">
+<a href={"mailto:"+specificProfile.basicInformation.email}> {specificProfile.basicInformation.email}  </a> <br />
+
+<a href={"tel:"+specificProfile.basicInformation.mobile}> {specificProfile.basicInformation.mobile}</a>
+                     </div>
+                
+                </div>
+                <div >
+                <div className="card2">
+                    <div className="card-bottom">
+                        <h2> Educational Qualifications</h2>
+                    </div>
+
+                    <div className="card-top">
+                        {specificProfile.education.map((i,j)=>(
+                        <ul>
+                            <li> {i.degree} </li>
+                        <p> {i.institution} </p>
+                        </ul>
+                        ))}
+                    </div>
+                </div>
+                <div className="card2">
+                    <div className="card-bottom">
+                        <h2> Technical Skills</h2>
+                    </div>
+
+                    <div className="card-top">
+                        {specificProfile.Technical.map((i,j)=>(
+                        <ul>
+                            <li> {i.Areas} </li>
+                        <p> {i.areadec} </p>
+                        </ul>
+                        ))}
+                    </div>
+                </div> 
+                </div>                          
+                
+
+
+
+            </section>
+            
+                
+            
+        )
+    }
+}
+
+export default Profile;
