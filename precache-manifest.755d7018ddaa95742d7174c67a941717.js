05d06eb8d6d95b23b480cd55a3b971d5ae557b33
matampalli satheesh <m.satheesh566@gmail.com>
2020-04-28 15:04:45 +0530

    Updates

---
+++ precache-manifest.755d7018ddaa95742d7174c67a941717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c5de8cf9bf7e9ed56a9b52be0a9a6b",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "b020333c60c5871570a2",
+    "url": "/react-profile/static/css/main.344046de.chunk.css"
+  },
+  {
+    "revision": "0f5b94cfe4a2492a9984",
+    "url": "/react-profile/static/js/2.c033a476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-profile/static/js/2.c033a476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b020333c60c5871570a2",
+    "url": "/react-profile/static/js/main.b19de513.chunk.js"
+  },
+  {
+    "revision": "9ed281195f38b6cb54f7",
+    "url": "/react-profile/static/js/runtime-main.94118734.js"
+  }
+]);
